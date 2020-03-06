6abb8dc4ec15af7dafc27777593f2d29293cd385
Defrian Yarfi <defrian.yarfi@sehatq.com>
2020-03-06 23:54:40 +0700

    Remove docs and attempt move to zeit

--- next.config.js
+++ next.config.js
@@ -3,9 +3,4 @@ module.exports = withSass({
   cssLoaderOptions: {
     importLoaders: 2
   },
-  publicRuntimeConfig: {
-    PORT: process.env.PORT,
-    ENV: process.env.ENV,
-    ENV_DEFAULT: "production"
-  }
 });
